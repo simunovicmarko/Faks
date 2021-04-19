f0c18013e21fd0cf294fe90eb25b06bd801ddbbb
simunovicmarko <59643387+simunovicmarko@users.noreply.github.com>
2021-04-19 11:43:08 +0200

    Ne vem ka vse je novega

---
+++ Sistemi odločanja/Projektna naloga/ahp_so/src/Components/Tree.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+
+interface Props {
+    
+}
+
+export const Tree:FunctionComponent<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
